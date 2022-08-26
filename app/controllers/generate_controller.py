c2d6aaf6bf16ba0edf3823f371d5224e88cd9487
GermanMT <germanoctako@gmail.com>
2022-08-26 18:17:28 +0200

    chore: Refactor code in controllers

---
+++ app/controllers/generate_controller.py
@@ -0,0 +1,102 @@
+from bson import ObjectId
+
+from app.apis.pypi_service import requires_packages, get_all_versions
+
+from app.controllers.version_controller import filter_versions_db
+
+from app.services.package_edge_service import add_package_edge, update_package_edge_versions
+from app.services.package_service import add_package, get_package_by_name, update_package_versions
+from app.services.version_service import add_version, get_version_by_release_and_date, update_version_package_edges
+
+from app.utils.ctc_parser import parse_constraints
+
+async def generate_packages(package_name: str, release: dict) -> list[ObjectId]:
+    require_packages = requires_packages(package_name, release)
+    version_package_edges = []
+
+    if require_packages:
+
+        for require_package in require_packages.items():
+            package_edge: dict = {}
+
+            name, raw_constraints = require_package
+            name = name.lower()
+            constraints = parse_constraints(raw_constraints)
+
+            package_edge['constraints'] = constraints
+
+            package = await get_package_by_name(name)
+            
+            if package is not None:
+
+                package_edge_id = await exist_package(package_edge, package)
+
+            else:
+
+                package_edge_id = await no_exist_package(name, package_edge, constraints)
+
+            version_package_edges.append(package_edge_id)
+
+    return version_package_edges
+
+
+async def exist_package(package_edge: dict, package: dict) -> ObjectId:
+    package_edge['package'] = package['_id']
+
+    filtered_versions = await filter_versions_db(package_edge['constraints'], package['versions'])
+
+    package_edge['versions'] = filtered_versions
+
+    new_package_edge = await add_package_edge(package_edge)
+
+    return new_package_edge['_id']
+
+async def no_exist_package(name: str, package_edge: dict, constraints: list[list[str]]) -> ObjectId:
+    package = {
+        'name': name
+    }
+
+    new_package = await add_package(package)
+
+    package_edge['package'] = new_package['_id']
+
+    new_package_edge = await add_package_edge(package_edge)
+
+    package_edge_id = new_package_edge['_id']
+
+    await generate_versions(new_package, package_edge_id, constraints)
+
+    return package_edge_id
+
+async def generate_versions(package: dict, package_edge_id: ObjectId, constraints: list) -> None:
+    package_versions = []
+    package_name = package['name']
+    package_id = package['_id']
+
+    all_versions = get_all_versions(package_name)
+
+    for version in all_versions:
+
+        existing_version = await get_version_by_release_and_date(version['release'], version['release_date'])
+
+        if existing_version is not None:
+
+            package_versions.append(existing_version['_id'])
+
+        else:
+
+            new_version = await add_version(version)
+
+            new_version_id = new_version['_id']
+
+            package_versions.append(new_version_id)
+
+            version_package_edges = await generate_packages(package['name'], new_version['release'])
+
+            await update_version_package_edges(new_version_id, version_package_edges)
+
+    filtered_versions = await filter_versions_db(constraints, package_versions)
+
+    await update_package_versions(package_id, package_versions)
+
+    await update_package_edge_versions(package_edge_id, filtered_versions)
--- app/models/package_edge_model.py
+++ app/models/package_edge_model.py
@@ -1,21 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 # from app.models.package_model import PackageModel
 # from app.models.version_model import VersionModel
 
 
 class PackageEdgeModel(BaseModel):
-    constraints: list[str] | None = None
+    constraints: list[list[str]] | str = Field(...)
     # PackageModel | None
     package = None
     # list[VersionModel] | None
     versions = None
-    schema_extra = {
-            'example': {
-                'constraints': ['<=0.7.0', '==1.2.1', '>2.3'],
-            }
-        }
 
     class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
+        schema_extra = {
+            'example': {
+                'constraints': [['<=', '0.7.0'], ['==', '1.2.1'], ['>', '2.3']],
+            }
+        }
