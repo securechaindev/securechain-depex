3a4fece10d74d4724b9bcb715fc5d834ca736c00
GermanMT <germanoctako@gmail.com>
2022-09-09 19:15:58 +0200

    chore: Refactor get first pos method

--- app/utils/get_first_pos.py
+++ app/utils/get_first_pos.py
@@ -1,6 +1,24 @@
+operators = ('<', '>', '=', '!', '|', '^', '~')
+
 async def get_first_position(data: str) -> int:
+    if not await have_any_op(data):
+        return len(data)
+
+    return await search_position(data)
+
+async def have_any_op(data: str) -> bool:
+    have_any_op = False
+
+    for op in operators:
+        if op in data:
+            have_any_op = True
+            break
+    
+    return have_any_op
+
+async def search_position(data: str) -> int:
     for char in data:
-        if char in ('<', '>', '=', '!', '|', '^', '~'):
+        if char in operators:
             return data.index(char)
 
-    return len(data)
+    return 0
