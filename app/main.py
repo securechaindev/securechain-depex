655fd4142443639fd9d4f9bd07ef5fed9bf41706
GermanMT <germanoctako@gmail.com>
2022-09-22 13:50:19 +0200

    style: Add info to API

--- app/main.py
+++ app/main.py
@@ -5,7 +5,22 @@ from starlette.middleware.cors import CORSMiddleware
 from app.router import api_router
 
 
-app = FastAPI()
+description = 'A simple backend for dependency extraction'
+
+app = FastAPI(
+    title="Depex",
+    description=description,
+    version="0.1.0",
+    contact={
+        "name": "Antonio Germán Márquez Trujillo",
+        "url": "https://github.com/GermanMT",
+        "email": "amtrujillo@us.es",
+    },
+    license_info={
+        "name": "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "url": "https://www.gnu.org/licenses/gpl-3.0.html",
+    },
+)
 
 # Set all CORS enabled origins
 app.add_middleware(
