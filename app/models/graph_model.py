674b5fbf7e2c3cfa526edab19c40ec0bfbaccf24
GermanMT <germanoctako@gmail.com>
2022-09-22 13:31:10 +0200

    feat: Add validators to graph model

--- app/models/graph_model.py
+++ app/models/graph_model.py
@@ -4,8 +4,8 @@ from app.models.requirement_file_model import RequirementFile
 
 
 class GraphModel(BaseModel):
-    owner: str = Field(...)
-    name: str = Field(...)
+    owner: str = Field(..., min_length = 1, description = "The owner repository size must be greater than zero")
+    name: str = Field(..., min_length = 1, description = "The name repository size must be greater than zero")
     add_extras: bool = Field(...)
     is_complete: bool = Field(...)
     requirement_files: list[RequirementFile] | None = None
