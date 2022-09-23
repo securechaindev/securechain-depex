ec1d258b5345505f00d1e6a4a71125806d8b01f1
GermanMT <germanoctako@gmail.com>
2022-09-23 11:56:28 +0200

    fix: Solving minor problems

--- app/controllers/graph_controller.py
+++ app/controllers/graph_controller.py
@@ -8,9 +8,16 @@ from app.controllers.generate_controller import exist_package, no_exist_package
 
 from app.models.graph_model import GraphModel
 
-from app.services.graph_service import read_graph, create_graph, update_graph_requirement_files, update_graph_is_completed
-from app.services.package_service import get_package_by_name
-from app.services.requirement_file_service import add_requirement_file, update_requirement_file
+from app.services.graph_service import (
+    read_graph, create_graph,
+    update_graph_requirement_files,
+    update_graph_is_completed
+)
+from app.services.package_service import read_package_by_name
+from app.services.requirement_file_service import (
+    create_requirement_file,
+    update_requirement_file
+)
 
 from app.utils.json_encoder import JSONEncoder
 
@@ -30,6 +37,7 @@ async def init_graph(background_tasks: BackgroundTasks, graph: GraphModel = Body
     graph_json = jsonable_encoder(graph)
     try:
         new_graph = await create_graph(graph_json)
+        background_tasks.add_task(generate_graph, new_graph)
         return JSONResponse(status_code = status.HTTP_201_CREATED, content = JSONEncoder().encode(new_graph))
     except HTTPException as error:
         return JSONResponse(status_code = error.status_code, content = JSONEncoder().encode({'message': error.detail}))
@@ -45,7 +53,7 @@ async def generate_graph(graph: dict) -> None:
 
         req_file = {'name': file[0], 'manager': file[1][0]}
 
-        new_req_file = await add_requirement_file(req_file)
+        new_req_file = await create_requirement_file(req_file)
 
         requirement_files.append(new_req_file['_id'])
 
@@ -55,7 +63,7 @@ async def generate_graph(graph: dict) -> None:
 
             package_name = dependencie[0]
 
-            package = await get_package_by_name(package_name)
+            package = await read_package_by_name(package_name)
 
             if package is not None:
 
--- app/controllers/version_controller.py
+++ app/controllers/version_controller.py
@@ -2,7 +2,7 @@ from bson import ObjectId
 
 from pkg_resources import parse_version
 
-from app.services.version_service import get_version_by_id
+from app.services.version_service import read_version_by_id
 
 from app.utils.operators import ops
 
@@ -14,7 +14,7 @@ async def filter_versions_db(constraints: list[list[str]], all_versions: list[Ob
     filter_versions = []
 
     for version_id in all_versions:
-        version = await get_version_by_id(version_id)
+        version = await read_version_by_id(version_id)
         checkers = [ops[constraint[0]](parse_version(version['release']), parse_version(constraint[1])) for constraint in constraints]
         if all(checkers): filter_versions.append(version_id)
 
