067a56b3a2e91bee390c54413d22e66b06827667
GermanMT <germanoctako@gmail.com>
2022-09-26 12:36:20 +0200

    fix: Solve problem with or ctc

--- app/utils/ctc_parser.py
+++ app/utils/ctc_parser.py
@@ -15,7 +15,7 @@ async def clean_constraints(raw_constraints: list[list[str]]) -> list[list[str]]
 
         if '||' in raw_constraint[0] or '||' in raw_constraint[1]:
             raw_constraint[0] = '!='
-            raw_constraint[1] = raw_constraint[1].split(' ')[0]
+            raw_constraint[1] = raw_constraint[1].split('|')[0]
         elif '*' in raw_constraint[1]:
             raw_constraint[0] = '~>'
             raw_constraint[1] = raw_constraint[1].replace('*', '0')
