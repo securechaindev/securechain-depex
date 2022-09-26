409f6e8ca66bd84ee6f710db1cebd93755008dfa
GermanMT <germanoctako@gmail.com>
2022-09-26 10:34:53 +0200

    feat: Add error handling

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError, ValidationError
+
+from json import loads
 
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
 
 from app.router import api_router
+
+from app.utils.json_encoder import JSONEncoder
 
 
 description = 'A simple backend for dependency extraction'
@@ -21,6 +27,16 @@ app = FastAPI(
         "url": "https://www.gnu.org/licenses/gpl-3.0.html",
     },
 )
+
+@app.exception_handler(RequestValidationError)
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(_ , exc):
+    exc_json = loads(exc.json())
+    response = {'message': []}
+    for error in exc_json:
+        response['message'].append(error['loc'][-1]+f": {error['msg']}")
+    
+    return JSONResponse(content = JSONEncoder().encode(response), status_code = 422)
 
 # Set all CORS enabled origins
 app.add_middleware(
