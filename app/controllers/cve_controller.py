c0b6f8b699e44f96939a032466905039021c8168
GermanMT <germanoctako@gmail.com>
2022-10-11 16:34:44 +0200

    feat: Use internal cves db instead nvd api

--- app/controllers/cve_controller.py
+++ app/controllers/cve_controller.py
@@ -1,41 +1,15 @@
-from dateutil.parser import parse
+from app.services.cve_service import read_cpe_matches_by_package_name
 
-from app.services.cve_service import (
-    create_cve,
-    read_cve_by_cve_id
-)
 from app.services.version_service import update_versions_cves_by_constraints
 
 
 async def extract_cves(package: dict) -> None:
-    # raw_cves = get_cves(package['name'])
-    raw_cves: dict = {'vulnerabilities': []}
+    cpe_matches = await read_cpe_matches_by_package_name(package['name'])
+    await relate_cve(cpe_matches, package)
 
-    for raw_cve in raw_cves['vulnerabilities']:
-        raw_cve = raw_cve['cve']
-        cve = await read_cve_by_cve_id(raw_cve['id'])
-        if cve:
-            cpe_matches = await get_cpe_matches(package['name'], cve)
-        else:
-            raw_cve['published'] = parse(raw_cve['published'])
-            raw_cve['lastModified'] = parse(raw_cve['lastModified'])
-
-            cve = await create_cve(raw_cve)
-
-            cpe_matches = await get_cpe_matches(package['name'], cve)
-        await relate_cve(cve, cpe_matches, package)
-
-async def get_cpe_matches(package_name: str, cve: dict) -> list:
-    cpe_matches = []
-    for configuration in cve['configurations']:
-        for node in configuration['nodes']:
-            for cpe_match in node['cpeMatch']:
-                if package_name in cpe_match['criteria']:
-                    cpe_matches.append(cpe_match)
-    return cpe_matches
-
-async def relate_cve(cve: dict, cpe_matches: list, package: dict) -> None:
-    for cpe_match in cpe_matches:
+async def relate_cve(cpe_matches: list, package: dict) -> None:
+    for raw_cpe_match in cpe_matches:
+        cpe_match = raw_cpe_match['configurations']['nodes']['cpeMatch']
         if (
             'versionStartIncluding' not in cpe_match and
             'versionEndIncluding' not in cpe_match  and
@@ -44,9 +18,9 @@ async def relate_cve(cve: dict, cpe_matches: list, package: dict) -> None:
         ):
             version = cpe_match['criteria'].split(':')[5]
             if '*' in version:
-                await update_versions_cves_by_constraints([], package['_id'], cve['_id'])
+                await update_versions_cves_by_constraints([], package['_id'], raw_cpe_match['_id'])
             else:
-                await update_versions_cves_by_constraints([['=', version]], package['_id'], cve['_id'])
+                await update_versions_cves_by_constraints([['=', version]], package['_id'], raw_cpe_match['_id'])
         else:
             ctcs = []
 
@@ -55,4 +29,4 @@ async def relate_cve(cve: dict, cpe_matches: list, package: dict) -> None:
             if 'versionStartExcluding' in cpe_match: ctcs.append(['>', cpe_match['versionStartExcluding']])
             if 'versionEndExcluding' in cpe_match: ctcs.append(['<', cpe_match['versionEndExcluding']])
 
-            await update_versions_cves_by_constraints(ctcs, package['_id'], cve['_id'])
+            await update_versions_cves_by_constraints(ctcs, package['_id'], raw_cpe_match['_id'])
--- app/services/dbs/indexes.py
+++ app/services/dbs/indexes.py
@@ -1,6 +1,6 @@
 from app.services.dbs.databases import (
     cve_collection,
-    version_collection
+    # version_collection
 )
 
 from pymongo import ASCENDING
@@ -8,14 +8,21 @@ from pymongo import ASCENDING
 
 async def create_indexes() -> None:
     await cve_collection.create_index('id', unique = True)
-
-    await version_collection.create_index(
+    await cve_collection.create_index(
         [
-            ('package', ASCENDING),
-            ('mayor', ASCENDING),
-            ('minor', ASCENDING),
-            ('pacth', ASCENDING),
-            ('build_numnber', ASCENDING)
+            ('id', ASCENDING),
+            ('configurations.nodes.cpeMatch.criteria', ASCENDING)
         ],
         unique = True
     )
+ 
+    # await version_collection.create_index(
+    #     [
+    #         ('package', ASCENDING),
+    #         ('mayor', ASCENDING),
+    #         ('minor', ASCENDING),
+    #         ('pacth', ASCENDING),
+    #         ('build_numnber', ASCENDING)
+    #     ],
+    #     unique = True
+    # )
