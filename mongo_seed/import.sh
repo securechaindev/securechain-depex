d978e617078168dc79e7222315985451b0ef6802
GermanMT <germanoctako@gmail.com>
2022-10-20 17:52:12 +0200

    feat: Change to mongorestore

--- mongo_seed/import.sh
+++ mongo_seed/import.sh
@@ -1,5 +1,5 @@
-mongoimport --host mongodb --db nvd --collection cves --file /mongo_seed/seeds/cves.json --jsonArray --batchSize 100000
-mongoimport --host mongodb --db depex --collection env_variables --file /mongo_seed/seeds/env_variables.json
-mongoimport --host mongodb --db pypi --collection package_edges --file /mongo_seed/seeds/package_edges.json --jsonArray --batchSize 100000
-mongoimport --host mongodb --db pypi --collection packages --file /mongo_seed/seeds/packages.json --jsonArray --batchSize 100000
-mongoimport --host mongodb --db pypi --collection versions --file /mongo_seed/seeds/versions.json --jsonArray --batchSize 100000
+mongorestore --host mongodb --gzip --db nvd --collection cves /mongo_seed/seeds/cves.bson.gz
+mongorestore --host mongodb --gzip --db depex --collection env_variables /mongo_seed/seeds/env_variables.bson.gz
+mongorestore --host mongodb --gzip --db pypi --collection package_edges /mongo_seed/seeds/package_edges.bson.gz
+mongorestore --host mongodb --gzip --db pypi --collection packages /mongo_seed/seeds/packages.bson.gz
+mongorestore --host mongodb --gzip --db pypi --collection versions /mongo_seed/seeds/versions.bson.gz
