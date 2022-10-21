5ddc30b1c5aebd18dfb26daa7edbcc999300c653
GermanMT <germanoctako@gmail.com>
2022-10-21 13:01:20 +0200

    fix: Add encoder for datatime objects

--- app/utils/json_encoder.py
+++ app/utils/json_encoder.py
@@ -2,9 +2,13 @@ from json import JSONEncoder
 
 from bson import ObjectId
 
+from datetime import datetime
+
 
 class JSONencoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime):
             return str(o)
         return JSONEncoder.default(self, o)
