87c7118361caab1165a5d39781fc464303743d88
GermanMT <germanoctako@gmail.com>
2022-11-05 00:45:06 +0100

    fix: Solve errors in empty repositories

--- app/apis/git_service.py
+++ app/apis/git_service.py
@@ -8,35 +8,38 @@ headers = {
     'Authorization': f'Bearer {settings.GIT_GRAPHQL_API_KEY}'
 }
 
-all_packages = {}
-
-async def get_repo_data(owner: str, name: str, end_cursor: str = '') -> dict[list, list] | list:
-    query = '{repository(owner: \"' + owner + '\", name: \"' + name + '\")'
-    query += '{dependencyGraphManifests{nodes{filename dependencies(after: \"' + end_cursor + '\"){pageInfo {hasNextPage endCursor}'
-    query += 'nodes{packageName requirements}}}}}}'
+async def get_repo_data(owner: str, name: str, all_packages: dict, end_cursor: str = None) -> dict[list, list] | list:
+    if not end_cursor:
+        query = '{repository(owner: \"' + owner + '\", name: \"' + name + '\")'
+        query += '{dependencyGraphManifests{nodes{filename dependencies{pageInfo {hasNextPage endCursor} nodes{packageName requirements}}}}}}'
+    else:
+        query = '{repository(owner: \"' + owner + '\", name: \"' + name + '\")'
+        query += '{dependencyGraphManifests{nodes{filename dependencies(after: \"' + end_cursor + '\"){pageInfo {hasNextPage endCursor}'
+        query += 'nodes{packageName requirements}}}}}}'
 
     session = await get_session()
     response = session.post('https://api.github.com/graphql', json={'query': query}, headers = headers, timeout = 50).json()
 
-    page_info = await json_reader(response)
+    page_info, all_packages = await json_reader(response, all_packages)
 
-    if not page_info['hasNextPage'] and end_cursor != '':
-        return []
+    if not page_info['hasNextPage']:
+        return all_packages
 
-    await get_repo_data(owner, name, page_info['endCursor'])
-    return all_packages
+    await get_repo_data(owner, name, all_packages, page_info['endCursor'])
 
-async def json_reader(response) -> tuple:
+async def json_reader(response, all_packages: dict) -> tuple:
+    page_info = {'hasNextPage': None}
+
     for node in response['data']['repository']['dependencyGraphManifests']['nodes']:
         file = node['filename']
         page_info = node['dependencies']['pageInfo']
         if file not in managers:
             continue
 
-        if file not in all_packages.keys():
+        if file not in all_packages:
             all_packages[file] = [managers[file], []]
         for node in node['dependencies']['nodes']:
             req = await parse_constraints(node['requirements'])
             all_packages[file][1].append([node['packageName'], req])
 
-    return page_info
+    return (page_info, all_packages)
