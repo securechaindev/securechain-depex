e765c5ea3427715827cacb5e63be87359f534ff5
GermanMT <germanoctako@gmail.com>
2023-02-06 16:58:49 +0100

    new seeds

--- mongo_seed/import.sh
+++ mongo_seed/import.sh
@@ -1,5 +1,5 @@
-mongorestore --host mongodb --gzip --db nvd --collection cves /mongo_seed/seeds/cves.bson.gz
-mongorestore --host mongodb --gzip --db depex --collection env_variables /mongo_seed/seeds/env_variables.bson.gz
-mongorestore --host mongodb --gzip --db pypi --collection package_edges /mongo_seed/seeds/package_edges.bson.gz
-mongorestore --host mongodb --gzip --db pypi --collection packages /mongo_seed/seeds/packages.bson.gz
-mongorestore --host mongodb --gzip --db pypi --collection versions /mongo_seed/seeds/versions.bson.gz
+mongorestore --host mongodb --gzip /mongo_seed/seeds/nvd/cves.bson.gz
+mongorestore --host mongodb --gzip /mongo_seed/seeds/depex/env_variables.bson.gz
+mongorestore --host mongodb --gzip /mongo_seed/seeds/pypi/package_edges.bson.gz
+mongorestore --host mongodb --gzip /mongo_seed/seeds/pypi/packages.bson.gz
+mongorestore --host mongodb --gzip /mongo_seed/seeds/pypi/versions.bson.gz
