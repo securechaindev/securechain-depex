05cfef9287c6e48e69f9eed66c89f1608c838c96
GermanMT <germanoctako@gmail.com>
2023-06-13 15:56:54 +0200

    feat: Use lru_cache with db sessions

--- app/services/dbs/databases.py
+++ app/services/dbs/databases.py
@@ -1,20 +1,46 @@
-from neo4j import AsyncGraphDatabase
+from functools import lru_cache
 
-from motor.motor_asyncio import AsyncIOMotorClient
+from neo4j import AsyncGraphDatabase, AsyncSession
+
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 
 from app.config import settings
 
-DRIVER = AsyncGraphDatabase.driver(
-    uri=settings.GRAPH_DB_URI,
-    auth=(settings.GRAPH_DB_USER, settings.GRAPH_DB_PASSWORD)
-)
 
-session = DRIVER.session()
+@lru_cache
+def get_graph_db_session(package_manager: str) -> AsyncSession:
+    pip_session = AsyncGraphDatabase.driver(
+        uri=settings.GRAPH_DB_URI_PIP,
+        auth=(settings.GRAPH_DB_USER, settings.GRAPH_DB_PASSWORD_PIP)
+    ).session()
+    npm_session = AsyncGraphDatabase.driver(
+        uri=settings.GRAPH_DB_URI_NPM,
+        auth=(settings.GRAPH_DB_USER, settings.GRAPH_DB_PASSWORD_NPM)
+    ).session()
+    mvn_session = AsyncGraphDatabase.driver(
+        uri=settings.GRAPH_DB_URI_MVN,
+        auth=(settings.GRAPH_DB_USER, settings.GRAPH_DB_PASSWORD_MVN)
+    ).session()
+    match package_manager:
+        case 'PIP':
+            return pip_session
+        case 'NPM':
+            return npm_session
+        case 'MVN':
+            return mvn_session
+        case 'ALL':
+            return [pip_session, npm_session, mvn_session]
+        case _:
+            return pip_session
 
-client = AsyncIOMotorClient(settings.VULN_DB_URI)
 
-depex_db = client.depex
-env_variable_collection = depex_db.get_collection('env_variables')
-
-nvd_db = client.nvd
-cve_collection = nvd_db.get_collection('cves')
+@lru_cache
+def get_collection(collection_name: str) -> AsyncIOMotorCollection:
+    client = AsyncIOMotorClient(settings.VULN_DB_URI)
+    match collection_name:
+        case 'env_variables':
+            return client.depex.get_collection('env_variables')
+        case 'cves':
+            return client.nvd.get_collection('cves')
+        case 'exploits':
+            return client.exploits.get_collection('exploits')
