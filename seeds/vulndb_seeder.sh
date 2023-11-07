5407e9ca99db689f1c9d60613fb461ff6dc0925c
GermanMT <germanoctako@gmail.com>
2023-11-08 00:30:48 +0100

    chore: Update seeds

--- seeds/vulndb_seeder.sh
+++ seeds/vulndb_seeder.sh
@@ -2,6 +2,8 @@
 
 export $(grep -v '^#' .env | xargs)
 
-mongorestore --nsInclude=cves.nvd --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
-mongorestore --nsInclude=exploits.exploit_db --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
+mongorestore --nsInclude=nvd.cves --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
+mongorestore --nsInclude=nvd.cpe_matchs --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
+mongorestore --nsInclude=nvd.cpes --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
+mongorestore --nsInclude=exploit_db.exploits --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
 mongorestore --nsInclude=depex.env_variables --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
