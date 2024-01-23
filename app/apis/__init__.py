cb043aa3c836fe94c7c01f770ae0f4ed7a241927
GermanMT <germanoctako@gmail.com>
2024-01-23 23:23:33 +0100

    fix: Remove useless githup api code

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -1,11 +1,10 @@
-from .github_service import get_last_commit_date_github, get_repo_data
+from .github_service import get_last_commit_date_github
 from .gitlab_service import get_last_commit_date_gitlab
 from .manager_service import get_all_versions, requires_packages
 
 __all__ = [
     "get_all_versions",
     "requires_packages",
-    "get_repo_data",
     "get_last_commit_date_github",
     "get_last_commit_date_gitlab",
 ]
--- app/apis/github_service.py
+++ app/apis/github_service.py
@@ -1,125 +1,15 @@
 from datetime import datetime
 from time import sleep
-from typing import Any
 
 from dateutil.parser import parse
 from requests import ConnectionError, ConnectTimeout, post
 
 from app.config import settings
-from app.utils import get_manager, parse_pip_constraints
 
 headers_github = {
     "Accept": "application/vnd.github.hawkgirl-preview+json",
     "Authorization": f"Bearer {settings.GITHUB_GRAPHQL_API_KEY}",
 }
-
-
-async def get_repo_data(
-    owner: str,
-    name: str,
-    all_packages: dict[str, Any] | None = None,
-    end_cursor: str | None = None,
-) -> dict[str, Any]:
-    if not all_packages:
-        all_packages = {}
-    if not end_cursor:
-        query = f"""
-        {{
-            repository(owner: "{owner}", name: "{name}") {{
-                dependencyGraphManifests {{
-                    nodes {{
-                        filename
-                        dependencies {{
-                            pageInfo {{
-                                hasNextPage
-                                endCursor
-                            }}
-                            nodes {{
-                                packageName
-                                requirements
-                            }}
-                        }}
-                    }}
-                }}
-            }}
-        }}
-        """
-    else:
-        query = f"""
-        {{
-            repository(owner: "{owner}", name: "{name}") {{
-                dependencyGraphManifests {{
-                    nodes {{
-                        filename
-                        dependencies (after: "{end_cursor}") {{
-                            pageInfo {{
-                                hasNextPage
-                                endCursor
-                            }}
-                            nodes {{
-                                packageName
-                                requirements
-                            }}
-                        }}
-                    }}
-                }}
-            }}
-        }}
-        """
-    while True:
-        try:
-            response = post(
-                "https://api.github.com/graphql",
-                json={"query": query},
-                headers=headers_github,
-            ).json()
-            break
-        except (ConnectTimeout, ConnectionError):
-            sleep(5)
-    page_info, all_packages = await json_reader(response, all_packages)
-    if not page_info["hasNextPage"]:
-        return all_packages
-    return await get_repo_data(owner, name, all_packages, page_info["endCursor"])
-
-
-async def json_reader(
-    response: Any, all_packages: dict[str, Any]
-) -> tuple[dict[str, Any], dict[str, Any]]:
-    page_info = {"hasNextPage": None}
-    for file_node in response["data"]["repository"]["dependencyGraphManifests"][
-        "nodes"
-    ]:
-        requirement_file_name = file_node["filename"]
-        if requirement_file_name == "package-lock.json":
-            continue
-        page_info = file_node["dependencies"]["pageInfo"]
-        requirement_file_manager = await get_manager(requirement_file_name)
-        if not requirement_file_manager:
-            continue
-        if requirement_file_name not in all_packages:
-            all_packages[requirement_file_name] = {
-                "package_manager": requirement_file_manager,
-                "dependencies": {},
-            }
-        for depependency_node in file_node["dependencies"]["nodes"]:
-            if requirement_file_manager == "MVN":
-                if "=" in depependency_node["requirements"]:
-                    depependency_node["requirements"] = (
-                        "["
-                        + depependency_node["requirements"]
-                        .replace("=", "")
-                        .replace(" ", "")
-                        + "]"
-                    )
-            if requirement_file_manager == "PIP":
-                depependency_node["requirements"] = await parse_pip_constraints(
-                    depependency_node["requirements"]
-                )
-            all_packages[requirement_file_name]["dependencies"].update(
-                {depependency_node["packageName"]: depependency_node["requirements"]}
-            )
-    return (page_info, all_packages)
-
 
 async def get_last_commit_date_github(owner: str, name: str) -> datetime:
     query = f"""
