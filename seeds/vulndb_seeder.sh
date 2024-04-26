2a03bb95ca4f7d09e9a57004b98997ee3e3ba5ca
GermanMT <germanoctako@gmail.com>
2024-04-26 11:39:27 +0200

    chore: Add vulners seeds

--- seeds/vulndb_seeder.sh
+++ seeds/vulndb_seeder.sh
@@ -6,5 +6,5 @@ mongorestore --nsInclude=nvd.cves --username $VULN_DB_USER --password $VULN_DB_P
 mongorestore --nsInclude=nvd.cpe_matchs --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
 mongorestore --nsInclude=nvd.cpe_products --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
 mongorestore --nsInclude=nvd.cpes --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
-mongorestore --nsInclude=exploit_db.exploits --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
+mongorestore --nsInclude=vulners_db.exploits --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
 mongorestore --nsInclude=depex.env_variables --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host localhost --gzip ./seeds/vuln/
