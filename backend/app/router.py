6813e073541763f918488c6436b46d7a5fb77987
GermanMT <germanoctako@gmail.com>
2024-07-20 14:46:48 +0200

    feat: Add helth in docker compose to backend

--- backend/app/router.py
+++ backend/app/router.py
@@ -5,9 +5,11 @@ from app.controllers import (
     config_operation_controller,
     file_operation_controller,
     graph_controller,
+    health_controller,
 )
 
 api_router = APIRouter()
+api_router.include_router(health_controller.router, tags=["health"])
 api_router.include_router(auth_controller.router, tags=["auth"])
 api_router.include_router(graph_controller.router, tags=["graph"])
 api_router.include_router(file_operation_controller.router, tags=["operation/file"])
