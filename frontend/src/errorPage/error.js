65556769e9ed82fffe911d376924a7405d1d11db
GermanMT <germanoctako@gmail.com>
2024-08-04 01:20:59 +0200

    fix: Solve warning with useState to get access token

--- frontend/src/errorPage/error.js
+++ frontend/src/errorPage/error.js
@@ -10,7 +10,7 @@ const PageNotFound = () => {
       </p>
       <p style={{ textAlign: 'center' }}>
         <Link className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' to='/'>
-          Go to Home{' '}
+          Go to Home
         </Link>
       </p>
     </div>
