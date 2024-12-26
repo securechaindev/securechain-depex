1ded28c83fe50f699ba232c5b4f805803300afec
GermanMT <germanoctako@gmail.com>
2024-12-26 11:56:03 +0100

    feat: Add PyPI all package manager dump

--- seeds/graphdb_seeder.sh
+++ seeds/graphdb_seeder.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export $(grep -v '^#' .env | xargs)
+neo4j-admin database load neo4j --from-path=/backups --overwrite-destination=true
 
-docker exec --interactive --tty pip_neo4j gzip -d /backups/pip_neo4j.cypher.gz
-docker exec --interactive --tty pip_neo4j cypher-shell -u $GRAPH_DB_USER -p $GRAPH_DB_PASSWORD_PIP -a bolt://localhost:7687 -f /backups/pip_neo4j.cypher > /dev/null 2>&1
-docker exec --interactive --tty npm_neo4j gzip -d /backups/npm_neo4j.cypher.gz
-docker exec --interactive --tty npm_neo4j cypher-shell -u $GRAPH_DB_USER -p $GRAPH_DB_PASSWORD_NPM -a bolt://localhost:7686 -f /backups/npm_neo4j.cypher > /dev/null 2>&1
-docker exec --interactive --tty mvn_neo4j gzip -d /backups/mvn_neo4j.cypher.gz
-docker exec --interactive --tty mvn_neo4j cypher-shell -u $GRAPH_DB_USER -p $GRAPH_DB_PASSWORD_MVN -a bolt://localhost:7685 -f /backups/mvn_neo4j.cypher > /dev/null 2>&1
+neo4j console
