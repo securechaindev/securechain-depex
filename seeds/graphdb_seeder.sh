8326d2e6eddfc2a2f5f5ce43530ce027335794b2
GermanMT <germanoctako@gmail.com>
2025-02-07 13:22:30 +0100

    feat: Add database not empty validation in Neo4J load script

--- seeds/graphdb_seeder.sh
+++ seeds/graphdb_seeder.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-neo4j-admin database load neo4j --from-path=/backups --overwrite-destination=true
+DB_PATH="/var/lib/neo4j/data/databases/neo4j"
+
+if [ -d "$DB_PATH" ] && [ "$(ls -A "$DB_PATH")" ]; then
+    echo "The database is not empty, no data will be imported"
+else
+    neo4j-admin database load neo4j --from-path=/backups
+fi
 
 neo4j console
--- seeds/vulndb_seeder.sh
+++ seeds/vulndb_seeder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-IS_EMPTY=$(mongosh --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb nvd --eval "db.getCollection('cves').countDocuments({}) === 0")
+HAVE_DATA=$(mongosh --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb nvd --eval "db.getCollection('cves').countDocuments({}) != 0")
 
-if [ $IS_EMPTY == true ]; then
+if [ $HAVE_DATA == true ]; then
+    echo "The database is not empty, no data will be imported"
+else
     mongorestore --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip --nsInclude=nvd.* --dir=./seeds/vuln/
     mongorestore --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip --nsInclude=depex.* --dir=./seeds/vuln/
-else
-  echo "The database is not empty, no data will be imported"
 fi
