66520242ba9f07d2c8eff4bf386149eb49401d05
GermanMT <germanoctako@gmail.com>
2025-08-11 19:46:08 +0200

    lint

--- app/utils/others/jwt_encoder.py
+++ app/utils/others/jwt_encoder.py
@@ -1,8 +1,7 @@
-# security.py
-from typing import Any, Dict
+from typing import Any
 
 from fastapi import HTTPException, Request, status
-from jwt import decode, ExpiredSignatureError, InvalidTokenError
+from jwt import ExpiredSignatureError, InvalidTokenError, decode
 
 from app.config import settings
 
@@ -11,7 +10,7 @@ class JWTBearer:
     def __init__(self, cookie_name: str = "access_token"):
         self.cookie_name = cookie_name
 
-    async def __call__(self, request: Request) -> Dict[str, Any]:
+    async def __call__(self, request: Request) -> dict[str, Any]:
         token = request.cookies.get(self.cookie_name)
         if not token:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
@@ -21,8 +20,14 @@ class JWTBearer:
                 settings.JWT_ACCESS_SECRET_KEY,
                 algorithms=[settings.ALGORITHM],
             )
-        except ExpiredSignatureError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
-        except InvalidTokenError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        except ExpiredSignatureError as err:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token expired",
+            ) from err
+        except InvalidTokenError as err:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token",
+            ) from err
         return payload
