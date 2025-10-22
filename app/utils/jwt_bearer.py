913ef70949424a2a026640903a703f85f12a3cf6
GermanMT <germanoctako@gmail.com>
2025-10-22 19:55:53 +0200

    feat: improve jwt bearer class

---
+++ app/utils/jwt_bearer.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from fastapi import Request
+from fastapi.security import HTTPBearer
+from jwt import ExpiredSignatureError, InvalidTokenError, decode
+
+from app.settings import settings
+from app.exceptions import (
+    ExpiredTokenException,
+    InvalidTokenException,
+    NotAuthenticatedException,
+)
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, cookie_name: str = "access_token"):
+        super().__init__(auto_error=False)
+        self.cookie_name = cookie_name
+
+    async def __call__(self, request: Request) -> dict[str, Any]:
+        token = request.cookies.get(self.cookie_name)
+        if not token:
+            raise NotAuthenticatedException()
+        try:
+            payload = decode(
+                token,
+                settings.JWT_ACCESS_SECRET_KEY,
+                algorithms=[settings.ALGORITHM],
+            )
+        except ExpiredSignatureError as err:
+            raise ExpiredTokenException() from err
+        except InvalidTokenError as err:
+            raise InvalidTokenException() from err
+        return payload
